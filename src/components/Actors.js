7a4d6594799e8e57797596e71cf783845ade7fb1
Andrianna <andrianna.odell@gmail.com>
2022-07-29 12:14:59 -0400

    reactor routes lab

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,27 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  
+  const actorInfo = actors.map((actor) => (
+    <div key = {actor.name}>
+      <h2>{actor.name}</h2>
+      {/* Create a map within a map to show the Actors movies */}
+      <ul>
+       {actor.movies.map((movie) => (
+        <li key = {movie}>
+          {movie}
+        </li>
+       ))}
+      </ul>
+    </div>
+  ))
+
+  return (
+  <div>
+    <h1>Actors Page</h1>
+    {actorInfo}
+  </div>
+  )
 }
 
 export default Actors;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,25 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+
+  const directorsList = directors.map((director)=> (
+    <div key={director.name}>
+      <h2>{director.name}</h2>
+      {/* Create a map within a map to show the directors movies */}
+      <ul>
+        {director.movies.map((movie) => (
+          <li key={movie}>{movie}</li>
+        ))}
+      </ul>
+    </div>
+  ))
+
+  return(
+    <div>
+      <h1>Directors Page</h1>
+      {directorsList}
+    </div>
+  )
 }
 
 export default Directors;
